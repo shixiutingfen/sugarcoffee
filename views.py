3aab3a45c1605ba5cb8c5c53e6c4540f2e49106e
shixiutingfen <345167830@qq.com>
2015-07-28 10:47:26 +0800

    链接

--- views.py
+++ views.py
@@ -1,17 +1,27 @@
 from myapp import app
 from flask import render_template
+
 @app.route('/')
-@app.route('/index')
-def index():
-	return "Hello, World!dsadsa"
-
-
 @app.route('/home')
 def home():
-    #user = {'nickname': 'Miguel'}
-    #posts=[{'author':'shixiuting','title':'husband'},{'author':'yangsaifen','title':'wife'}]
     return render_template("index.html",title = 'Home')
+
+@app.route('/about')
+def about():
+	return render_template("about.html",title = 'About')
 
 @app.route('/learn')
 def learn():
     return render_template("learn.html",title = 'learn')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template("portfolio4.html",title = 'portfolio')
+
+@app.route('/bloglist')
+def bloglist():
+    return render_template("blog.html",title = 'bloglist')
+
+@app.route('/blogdetail')
+def blogdetail():
+    return render_template("blog-single.html",title = 'blogdetail')
